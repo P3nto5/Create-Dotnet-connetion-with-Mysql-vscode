2d6d41d2771969a05b8ec2ac7f7da1b181508f12
samuel <sam226gold@gmail.com>
2019-03-01 16:31:48 -0400

    WebApi with dotnet(c#) and mysql

---
+++ agilAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using agilAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace agilAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options){}
+
+        public DbSet<Person> Persons{get; set;}
+    }
+}
